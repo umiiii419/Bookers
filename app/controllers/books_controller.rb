4899ba4f30149a31d0f5ff70cdd232b0a05ec9f6
umiiii419 <umiiii419@gmail.com>
2021-07-19 04:59:55 +0000

    [Add]3-8 createアクションの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,16 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 
 end
