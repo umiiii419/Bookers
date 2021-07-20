7762ab57090504cb15c10fbed1875a9691034c26
umiiii419 <umiiii419@gmail.com>
2021-07-20 00:14:13 +0000

    [Add]3-8 new,show,edit,update,destroyの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,35 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
+    redirect_to '/books/:id'
+    
+    if @book.save
+      redirect_to root_path, success: "Book was successfully created."
+      flash.now[:danger] = "登録に失敗しました"
+      render :new
+    end
+    
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
     redirect_to book_path(book.id)
   end
 
+   def destroy
+     book = Book.find(params[:id])
+     book.destroy
+     redirect_to books_path
+   end
 
   private
   def book_params
